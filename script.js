e206fcd396bbc732c6a3c821c1bf49ebafa35af1
kfrain <114533666+kfrain@users.noreply.github.com>
2023-06-24 17:26:05 -0400

    script.js
    New file has commented sections to give more context to the different functions

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function inputLength() {
 	return input.value.length;
 }
 
+//want button in front of each list item, additionally the id changes 
+//with the items being added so that they can be crossed out individually
 function createListElement() {
 	var li = document.createElement("li");
     var checkbox = document.createElement("input");
@@ -22,11 +24,12 @@ function createListElement() {
         checkbox.addEventListener('click', markItem);
       });
 }
-
+//this is for the static parts already on page (not included in create list item)
 document.querySelectorAll('input[type="checkbox"]').forEach(function(checkbox) {
     checkbox.addEventListener('click', markItem);
   });
 
+//this and the keypress allow the buttons to add items
 function addListAfterClick() {
 	if (inputLength() > 0) {
 		createListElement();
@@ -38,18 +41,23 @@ function addListAfterKeypress(event) {
 	createListElement();
     }
 }
-
+//this applies the strikethrough and greying out the "completed" items
+//when they are checked off
 function markItem(event) {
     event.target.parentNode.classList.toggle("done");
 }
 
-
+//We dont want each task to disappear when it's checked off, but also 
+//want to be able to clear list to add more items, this will clear all items
+//the while function will remove each firstchild while until there aren't any more
+//this effectively clears the list
 function clearTasks() {
     while (ul.firstChild) {
     ul.removeChild(ul.firstChild)
     }
 }
-
+//listeners waiting until there is user action with certain elements in
+//order to execute related functions
 button.addEventListener("click", addListAfterClick);
 
 clearButton.addEventListener("click", clearTasks);
